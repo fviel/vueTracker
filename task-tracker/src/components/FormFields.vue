c3606f3aeb4cd03bbd9edc06db32db1368bf295f
fviel <nandoviel@hotmail.com>
2022-04-23 22:22:55 -0300

    criados os primeiros componetes

---
+++ task-tracker/src/components/FormFields.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="box">
+    <div class="columns">
+      <div
+        class="column is-8"
+        role="form"
+        aria-label="Formulário para a criação de uma nova tarefa"
+      >
+        <input
+          type="Text"
+          class="input"
+          placeholder="Qual tarefa você deseja iniciar?"
+        />
+      </div>
+
+      <div class="column">
+        <section>
+          <strong>00:00:00</strong>
+        </section>
+
+        <button class="button">
+          <span class="icon">
+            <i class="fas fa-play"></i>
+          </span>
+          <span>Play</span>
+        </button>
+
+        <button class="button">
+          <span class="icon">
+            <i class="fas fa-stop"></i>
+          </span>
+          <span>Stop</span>
+        </button>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+export default defineComponent({
+    name: 'FormFields'
+})
+</script>
