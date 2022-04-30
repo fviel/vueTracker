84359623cd7b52efedd7a5aff80cf4a5a0c49394
fviel <nandoviel@hotmail.com>
2022-04-29 23:58:22 -0300

    passando o css dentro do javascript

--- task-tracker/src/components/TaskBox.vue
+++ task-tracker/src/components/TaskBox.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="box has-text-weight-bold">
+  <div class="box has-text-weight-bold" :style="estilos">
     <!-- o slot faz com que o conteúdo do outro componente seja renderizado dentro dele -->
     <slot></slot>
   </div>
@@ -8,14 +8,25 @@
 <script lang="ts">
 import { defineComponent} from "vue";
 export default defineComponent({
-  name: "TaskBox",   
+  name: "TaskBox",  
+  data(){
+    return{
+      estilos:{
+        backgroundColor:  '#b8d8eb'
+        //background-color: '#b8d8eb'
+      }
+    }
+  } 
 });
 </script>
 
 
-<!-- estilo escopado quer dizer que somente se aplica a este componente ao para os filhos dele -->
+<!-- estilo escopado quer dizer que somente se aplica a este componente ao para os filhos dele 
+Este aqui é o jeito mais comum de passar o estipo, mas usando o comando :stype, 
+posso passar ele dentro de script, o que permite maior flexibilidade de programação
 <style scoped>
 .box {
   background: #b8d8eb;
 }
 </style>
+-->
